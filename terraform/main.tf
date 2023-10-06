ab2e68f860fb3c73bdbbe6e934f21823d80f3f1b
Mihalevich Uladzimir <mihalevich.vladimir@gmail.com>
2023-10-06 16:48:00 +0000

    fix main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,9 +1,8 @@
 terraform {
-  required_version = ">= 1.1.6"
   required_providers {
     yandex = {
       source  = "yandex-cloud/yandex"
-#      version = ">= 0.87.0"
+      version = ">= 0.87.0"
     }
   }
 
